ca70f458c06f6d14945e4c22e6db2308a5a9e002
updjolo <56732692+updjolo@users.noreply.github.com>
2026-09-06 00:20:24 +0900

    Create gacha-server.js

---
+++ gacha-server.js
@@ -0,0 +1,13 @@
+{
+  "type": "service_account",
+  "project_id": "spaga-gacha",
+  "private_key_id": "fdb1f95e136b476583a7dc01e37b9931e3b00089",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCr/KoFlUttsO+R\n/SSAJMg9CDYIPRQxmntLNB/PL8kgYJMQ+oj1tPhenk3/STDZKVcJOV8s0AkZDxDE\nKXZJQFqvWhcngnhuhJb8Q37CYnMCUUN+Sq0D+2UQU2XNIv2jBUzPhV4yv1tROTrz\nJj3Bq3OnxMKDgQciT5/bMBQ09GzVVd3jtOF08hUr9wFbU0pWwEYiSXA/MwfsaF6h\nJqKPyThoVqqJ0x+9uso3QHk0hWjroaFgVz/vj4CG0VCQsr18wA/fYXcJyc7Krpgh\nNkMwp8MUUL2lBT1hZSBe3n6xjL7ZFTkqR0Nzu+HADYGg1YcB1/bKdcg+qR5B2Nch\nlRg6kCC5AgMBAAECggEAKukqBKWYwcANLggSdNR4j12+mXSf1rZ7/6n8K8X48RaX\navuPc3qZI4Zs4H6C7RPW7uq9F+G5+2IaZU8Q/7rKRYj9UPLJGI4FW+B9PoOwvUnI\nRLjeDSnKr/WW50VNMkDuxrI4bAH5hzz3u9X1P56HBztc6tGlFYb3VAldaU743osa\nzARy6rDi7yZ8LRBu8mEtiPGZJCBlswm0tSr0Xdd4TiMqYdb8m8ITzBnsWMrmKNdV\n7E55rju3vc/rQMVIPLYAxGhz9UVKIuAHK1fKIpzCggzRLWsvOuVkbXSmoNTBO/em\nI8bMVtV54p3nx140AnGM44WN2wYOQ2rDhxSLreALhwKBgQDWYKjsnGKFTOk/t+MO\n7pmnmh5c0iuvx85OUyMtPZV+9Ij5HfGjj32l+Zj28R7P8ZSFUvMxCPqXTXX9GT2y\nBdhSxSjfnoRHvPfoMZpXlSjUWcCcOdYGE+Y0+P1ixyA8Hix9GS8icqvoTIdJNS0g\n7Ai2OKurpnmSqhKsMLW7+DzudwKBgQDNYQmz9w3VfkLtVW4uUkEARVCYI4XdFl83\ncGREWCVllsmTQIn/9i3uS4uuVuuGq8tjnyp9uAWslnH0spLsm0M9p9uiwCGhMlSf\n9gNWNgXSvx0kHZxgI+lQIaQknuqihClmee2c83nQUkQrS+ZYCBYLMGThrfT0sUD/\niXSM+2JGTwKBgEb6vBOTd99Q5s2tEeXIxGFE2xa6k7JgwoMWbcg14sClMJDjBDW2\nddC1gpsInL4W66j1HQY4RdhA0ncZm17NJqHJG85mir/B5ocfnU2wWlNs4CQZ1i4u\n+oLBigvhiF+/UdWduAh81URVyllH3QhQflXvD5AGz6vq2cHfYty4xVvjAoGBALvB\n/Obj82e8WF80/NeoJviiZb3Qq+6lJ0M9oszAcN/g7EOMexzf6Lmzou0tVYD2yag3\nuNbaR4WyvFdiceORRfW14Zt2XshJhQgAFqjYFpbGVVPRJkbWTdQODUvVZNhSTq76\nxPn5tOF4dA3JA7pzGgP8qB2RtSGGcs+q0DybDQsNAoGBAKbIeVLd4TymLYtT5y65\neK4StHTmzeC/9ZwUyNyuLzoIA5BKCTHvTexRQCeIvGil/V7W/m01ZjQTnushrl7M\nPUo0knprsLCbA38VhGble7A3ZV3tNzvhkubGiKBjX6vOAKD2BiwmhfJw5v0pG0OQ\nYl1pxem9Cd3rdcqGNVZLYFKV\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@spaga-gacha.iam.gserviceaccount.com",
+  "client_id": "103790629367697220799",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40spaga-gacha.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
